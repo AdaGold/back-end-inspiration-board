2710ece91ecc3bec0dfc3cfac9692f86b55ae809
mana <manushka@pm.me>
2021-06-28 15:51:19 -0700

    creates models

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,6 @@
 from app import db
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
