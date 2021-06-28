f2a7906ab144b48c80072b879a150ad8417516c1
alison <alimcginley@gmail.com>
2021-06-28 12:41:03 -0700

    initial commit w models

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,8 @@ def create_app():
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
         "SQLALCHEMY_DATABASE_URI")
-
+    from app.models.board import Board
+    from app.models.card import Card
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
 
