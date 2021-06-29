d486031e194b784f8f741078e4fafdc0469d62ac
marielaxcruz <marielacrtzcruz@gmail.com>
2021-06-28 18:05:42 -0700

    completed the back-end layer setup

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,8 @@ def create_app():
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
+    from app.models.board import Board
+    from app.models.card import Card
 
     db.init_app(app)
     migrate.init_app(app, db)
--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,12 @@
 from app import db
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
+
+    def get_return(self):
+        return{
+                    "board_id": self.board_id,
+                    "title": self.title,
+                }
