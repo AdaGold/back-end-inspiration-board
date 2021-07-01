800259409a6f23fc86150772603ce7d0ecfc175c
sneha agarwal <sneha8412@gmail.com>
2021-06-30 22:45:14 -0700

    added support to sort get_all_cards by asc or desc order of likes count

--- app/card_routes.py
+++ app/card_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, make_response, Response
 from app import db
 from .models.card import Card
 from .models.board import Board
+from sqlalchemy import desc, asc
 
 # example_bp = Blueprint('example_bp', __name__)
 card_bp = Blueprint("/board/<board_id>/cards", __name__, url_prefix="/board/<board_id>/cards")
@@ -37,24 +38,37 @@ def create_a_card(board_id):
 @card_bp.route("", methods=["GET"], strict_slashes=False)
 def get_all_cards(board_id):
     
-    board = Board.query.get(board_id)
+    # get sort query param
+    sort_by_like_count_order = request.args.get("sort");
+    
+    # board = Board.query.get(board_id)
     
     #print(f"get_all_cards(): Board = {board}")
     
-    if not board or board == None:
-        return jsonify(""), 404 
+    #if not board or board == None:
+    #    return jsonify(""), 404 
     
     cards_list = []
     
-    board_cards = board.cards
+    # board_cards = board.cards
     
     #print(f"get_all_cards(): board.cards = {board_cards}")
     
-    for card in board.cards:
-        card_data = Card.query.get(card.card_id)
-        cards_list.append(card_data.to_json())
+    # for card in board.cards:
+    #     card_data = Card.query.get(card.card_id)
+    #     cards_list.append(card_data.to_json())
     
-    return make_response(jsonify(cards_list), 200)         
+    if sort_by_like_count_order is not None:
+        if (sort_by_like_count_order == "asc"):
+            cards_list = db.session.query(Card).filter_by(board_id=board_id).order_by(asc(Card.likes_count)) 
+        else:
+            cards_list = db.session.query(Card).filter_by(board_id=board_id).order_by(desc(Card.likes_count))
+    else:
+        cards_list = db.session.query(Card).filter_by(board_id=board_id).all()
+    
+    cards_json_list = [ card.to_json() for card in cards_list ]
+    
+    return make_response(jsonify(cards_json_list), 200)         
 
 
 @card_bp.route("<card_id>", methods=["DELETE"], strict_slashes=False)
