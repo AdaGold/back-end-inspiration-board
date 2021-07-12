3b5cf0ab46ec0732dda085a307832dd0eebd2ea4
Christina <206peacegypsy@gmail.com>
2021-07-12 15:09:53 -0700

    initial setup and add models

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,9 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
         "SQLALCHEMY_DATABASE_URI")
 
-    # Import models here for Alembic setup
-    # from app.models.ExampleModel import ExampleModel
+
+    from app.models.board import Board
+    from app.models.card import Card
 
     db.init_app(app)
     migrate.init_app(app, db)
--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,6 @@
 from app import db
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
