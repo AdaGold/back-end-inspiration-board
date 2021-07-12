05a0951a9d51b58765629350723aaa194b1ff9c3
Dena Burd <denaburd@gmail.com>
2021-07-12 15:19:45 -0700

    Moves models folder insie app

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,7 @@
 from app import db
+
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
