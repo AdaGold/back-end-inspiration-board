00941b0ba03552b4be7e2145f5600fb0a5b763a1
nathalia <araujo.nathalia03@gmail.com>
2021-07-15 12:35:32 -0700

    Add post_message using request

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.models.card import Card
 import os
 from dotenv import load_dotenv
 import requests
-from slack_sdk import WebClient
+# import slack_sdk
 from slack_sdk.errors import SlackApiError
 
 boards_bp = Blueprint("boards", __name__, url_prefix="/boards")
@@ -14,25 +14,30 @@ hello_world_bp = Blueprint("hello_world", __name__)
 
 load_dotenv()
 
-#Basic route to test if the server is running
+# Basic route to test if the server is running
+
+
 @hello_world_bp.route("/hello-world", methods=["GET"])
 def hello_world():
     my_beautiful_response_body = "Hello, World!"
     return my_beautiful_response_body
 
-#method to post message to slack
+# method to post message to slack
+
+
 def post_message_to_slack(text):
     SLACK_TOKEN = os.environ.get('SLACKBOT_TOKEN')
-    client = WebClient(token=SLACK_TOKEN)
-    try:
-        response = client.chat_postMessage(
-            channel='C0286U213J5',
-            text=text
-        )
-    except SlackApiError as e:
-        assert e.response["error"]
+    slack_path = "https://slack.com/api/chat.postMessage"
+    query_params = {
+        'channel': 'C0286U213J5',
+        'text': text
+    }
+    headers = {'Authorization': f"Bearer {SLACK_TOKEN}"}
+    requests.post(slack_path, params=query_params, headers=headers)
 
-#routes for getting all boards and creating a new board
+# routes for getting all boards and creating a new board
+
+
 @boards_bp.route("", methods=["GET", "POST"])
 def handle_boards():
     if request.method == "GET":
@@ -58,7 +63,9 @@ def handle_boards():
 
     return make_response(f"Board {new_board.title} successfully created", 201)
 
-#routes for getting a specific board, updating a board, and deleting a board
+# routes for getting a specific board, updating a board, and deleting a board
+
+
 @boards_bp.route("/<board_id>", methods=["GET", "PUT", "DELETE"])
 def handle_board(board_id):
     board = Board.query.get_or_404(board_id)
@@ -74,7 +81,7 @@ def handle_board(board_id):
             "title": board.title,
             "owner": board.owner,
             "cards": cards
-      })
+        })
     elif request.method == "PUT":
         if board == None:
             return make_response("Board does not exist", 404)
@@ -95,7 +102,9 @@ def handle_board(board_id):
         return make_response(f"Board: {board.title} sucessfully deleted.")
 # example_bp = Blueprint('example_bp', __name__)
 
-#route for getting all cards in a board and making a new card
+# route for getting all cards in a board and making a new card
+
+
 @boards_bp.route("/<board_id>/cards", methods=["POST", "GET"])
 def handle_cards(board_id):
     board = Board.query.get(board_id)
@@ -119,7 +128,7 @@ def handle_cards(board_id):
         request_body = request.get_json()
         if 'message' not in request_body:
             return {"details": "Invalid data"}, 400
-        
+
         new_card = Card(message=request_body["message"],
                         board_id=board_id)
 
@@ -135,7 +144,9 @@ def handle_cards(board_id):
             }
         }, 201
 
-#route for deleting a card     
+# route for deleting a card
+
+
 @boards_bp.route("/<board_id>/<card_id>", methods=["DELETE"])
 def handle_card(board_id, card_id):
     card = Card.query.get_or_404(card_id)
@@ -145,4 +156,3 @@ def handle_card(board_id, card_id):
 
     return make_response(
         f"Card Message: {card.message} Card ID: {card.card_id} deleted successfully")
-
