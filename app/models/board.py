c7d32ee59b85e194921165a0a54cf248f44c33e2
roza ajema <mentoroza@yahoo.com>
2021-12-23 09:44:41 -0800

    creates one to many relationship between models

--- app/models/board.py
+++ app/models/board.py
@@ -1,8 +1,9 @@
 from app import db
 
 class Board (db.Model):
-  board_id = db.Column (db.Integer, primary_key = True)
+  board_id = db.Column (db.Integer, primary_key = True, autoincrement=True)
   title =  db.Column (db.String)
   owner =  db.Column (db.String)
+  cards = db.relationship("Card", back_populates="board")
    
 
--- app/models/card.py
+++ app/models/card.py
@@ -1,6 +1,7 @@
 from app import db
 
 class Card (db.Model):
-  card_id = db.Column(db.Integer, primary_key = True)
+  card_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
   message = db.Column(db.String)
   likes_count = db.Column(db.Integer)
+  board = db.relationship("Board", back_populates="cards")
