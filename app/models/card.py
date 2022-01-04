5b234902a16f855f590d19e8e4785fbf8d19ebf8
Laurel S <ls60@protonmail.com>
2022-01-04 01:23:08 -0800

    Connects card and board records with foreign key

--- app/models/card.py
+++ app/models/card.py
@@ -4,4 +4,5 @@ class Card (db.Model):
   card_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
   message = db.Column(db.String)
   likes_count = db.Column(db.Integer)
+  board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'))
   board = db.relationship("Board", back_populates="cards")
