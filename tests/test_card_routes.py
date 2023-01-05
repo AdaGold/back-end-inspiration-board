f3862b8e5d6fc4e00b1c6093c3b4e39b18e0b04f
Alaere00 <alaere@pdx.edu>
2023-01-05 12:01:30 -0800

    Slack message complete moving onto to final board routes

--- tests/test_card_routes.py
+++ tests/test_card_routes.py
@@ -1,3 +1,39 @@
 from app.models.card import Card
 import pytest
 
+@pytest.mark.skip(reason="incomplete")
+def test_no_cards_saved(client):
+    # Act
+    response = client.get("/cards")
+    response_body = response.get_json()
+
+    # Assert 
+    assert response.status_code == 200
+    assert response_body == []
+
+@pytest.mark.skip(reason="incomplete")
+def test_get_cards_one_saved_card(client, one_card):
+    # Act
+    response = client.get("/cards")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [{
+        {
+            "card_id": None,
+            "likes": None, 
+            "message": None,
+        }
+    }
+    ]
+
+@pytest.mark.skip(reason="incomplete")
+def test_get_card(client, one_card):
+    # Act
+    response = client.get("/cards/1")
+    response_body = response.get_json()
+
+    # Assert 
+
+# @pytest.mark.skip(reason="incomplete")
