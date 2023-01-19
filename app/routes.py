de98c43113eedff55039450a1db7fbaff266ba89
Alaere00 <alaere@pdx.edu>
2023-01-19 09:29:33 -0800

    small fix in routes

--- app/routes.py
+++ app/routes.py
@@ -143,17 +143,10 @@ def delete_card(board_id, card_id):
     
     return make_response(jsonify({"details": f"Card {card_id} \"{card.message}\" successfully deleted"}), 200)
 
+
 # @board_bp.route("/<board_id>/cards/<card_id>", methods=["PATCH"])
 # def update_likes_in_card(board_id, card_id):
 #     card = validate_model(Card, card_id)
-    resp = make_response(jsonify({"details": f"Card {card_id} \"{card.message}\" successfully deleted"}), 200)
-    resp.headers["Access-Control-Allow-Origin"] = "*"
-    resp.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    resp.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    return resp
-@board_bp.route("/<board_id>/cards/<card_id>", methods=["PATCH"])
-def update_likes_in_card(board_id, card_id):
-    card = validate_model(Card, card_id)
 
 #     if card is None:
 #         return make_response(jsonify({"error": "Card not found"}), 400)
