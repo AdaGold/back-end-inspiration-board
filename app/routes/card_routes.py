cebf11a17395bc718f41162ce7990744e54b61f5
vlada <vladarap88@gmail.com>
2024-12-23 21:38:04 -0800

    added vladite_model to route_utilities file and created update api for card

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -1,13 +1,13 @@
-
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.board import Board
 from app.models.card import Card
+from .route_utilities import validate_model
 from ..db import db
 import requests
 import json
 import os
 
-bp=Blueprint("cards_bp", __name__, url_prefix="/boards/<board_id>/cards")
+bp = Blueprint("cards_bp", __name__, url_prefix="/boards/<board_id>/cards")
 
 
 @bp.post("")
@@ -15,16 +15,17 @@ def create_card(board_id):
 
     request_body = request.get_json()
 
-   
     try:
         new_card = Card.from_dict(request_body)
     except KeyError as e:
         missing_key = e.args[0]
         response = {"details": f"Invalid request body: Missing key '{missing_key}'"}
         abort(make_response(response, 400))
-    
+
     if len(request_body["message"]) > 40:
-        response = {"details": "Invalid request body: Message must be 40 characters or less"}
+        response = {
+            "details": "Invalid request body: Message must be 40 characters or less"
+        }
         abort(make_response(response, 400))
 
     db.session.add(new_card)
@@ -32,3 +33,18 @@ def create_card(board_id):
 
     response = {"card": new_card.to_dict()}
     return response, 201
+
+
+@bp.put("/<card_id>")
+def agree_with_card(card_id):
+    try:
+        card = validate_model(Card, card_id)
+    except:
+        response = {"details": f"Card {card_id} not found"}
+        abort(make_response(response, 404))
+
+    card.likes_count += 1
+    db.session.commit()
+
+    response = {"card": card.to_dict()}
+    return response
