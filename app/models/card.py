70d4c2d5df99083d888e75d5165383dd91d7b296
Wei <qwbu22@gmail.com>
2024-12-23 20:25:37 -0800

    Add from_dict and to_dict methods to Card model; update board_routes to handle Board creation

--- app/models/card.py
+++ app/models/card.py
@@ -11,3 +11,21 @@ class Card(db.Model):
     likes_count: Mapped[int]
     board_id: Mapped[Optional[int]] = mapped_column(ForeignKey("board.id"))
     board: Mapped[Optional["Board"]] = relationship(back_populates="cards")
+
+    def from_dict(request_body):
+        new_card = Card(
+            message=request_body["message"],
+            likes_count=request_body["likes_count"],
+            board_id=request_body["board_id"]
+        )
+        return new_card
+
+    def to_dict(self):
+        new_card ={
+            "id": self.id,
+            "message": self.message,
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
+        }
+        return new_card
+    
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -15,20 +15,18 @@ def create_board():
 
    
     try:
-        new_card = Card.from_dict(request_body)
+        new_board = Board.from_dict(request_body)
     except KeyError as e:
         missing_key = e.args[0]
         response = {"details": f"Invalid request body: Missing key '{missing_key}'"}
         abort(make_response(response, 400))
     
-    if len(request_body["message"]) > 40:
-        response = {"details": "Invalid request body: Message must be 40 characters or less"}
-        abort(make_response(response, 400))
 
-    db.session.add(new_card)
+
+    db.session.add(new_board)
     db.session.commit()
 
-    response = {"card": new_card.to_dict()}
+    response = {"board": new_board.to_dict()}
     return response, 201
 
 
